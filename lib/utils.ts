da117ebe1552a407ff669b5b52c2c237c1067deb
Mayursinh <marvinduff97@gmail.com>
2020-01-31 17:58:09 +0530

    add isGarbage to check null or undefined

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,11 @@ const addColonLast = (url: string): boolean => (
   url.indexOf('/') === -1
 );
 
+const isGarbage = (value: any): boolean => (
+  value === null || value === undefined
+);
+
 module.exports = {
   addColonLast,
+  isGarbage,
 };
