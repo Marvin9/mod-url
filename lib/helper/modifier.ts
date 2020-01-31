2e0560b5546237521acf50661d42dfc705b7ef62
Mayursinh <marvinduff97@gmail.com>
2020-01-31 16:26:20 +0530

    add garbage input checker

--- lib/helper/modifier.ts
+++ lib/helper/modifier.ts
@@ -1,27 +1,33 @@
 import { modType } from '../../config/interfaces';
 
+const { isGarbage } = require('../utils');
+
 function protocol(prtcl: string): modType {
+  if (isGarbage(prtcl)) return this;
   this.parsedurl.protocol = prtcl;
   return this;
 }
 
 function subdomain(sbdm: string): modType {
+  if (isGarbage(sbdm)) return this;
   this.parsedurl.subdomain = sbdm;
   return this;
 }
 
 function domain(dm: string): modType {
+  if (isGarbage(dm)) return this;
   this.parsedurl.domain = dm;
   return this;
 }
 
 function domainext(ext: string): modType {
+  if (isGarbage(ext)) return this;
   this.parsedurl.domainext = ext;
   return this;
 }
 
 function port(prt: string | number): modType {
-  if (prt === null || prt === undefined) return this;
+  if (isGarbage(prt)) return this;
   this.parsedurl.port = `${prt}`;
   return this;
 }
