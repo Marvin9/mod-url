bfc0e8a8541a67a72bdc24fa3600579cb2caf1b1
Mayursinh <marvinduff97@gmail.com>
2020-02-02 09:51:59 +0530

    :white_check_mark: Add parse function test

--- tests/parse.test.js
+++ tests/parse.test.js
@@ -45,3 +45,57 @@ test('it should add protocol if not present', () => {
   const google = mod.parse('google.com');
   expect(google.parsedurl.protocol).toBe('https');
 });
+
+test('it should identify middle (hostname)', () => {
+  const github = mod.parse('https://google.com');
+  expect(github.parsedurl.domain).toBe('google');
+  expect(github.parsedurl.domainext).toBe('com');
+  expect(github.parsedurl.subdomain).toBe('www'); // default assign www
+
+  const google = mod.parse('http://www.facebook.uk/x/y');
+  expect(google.parsedurl.domain).toBe('facebook');
+  expect(google.parsedurl.subdomain).toBe('www');
+  expect(google.parsedurl.domainext).toBe('uk');
+
+  const multipleDot = mod.parse('x.y.z.google.com');
+  expect(multipleDot.parsedurl.domain).toBe('google');
+  expect(multipleDot.parsedurl.subdomain).toBe('x.y.z');
+  expect(multipleDot.parsedurl.domainext).toBe('com');
+
+  const zeroDot = mod.parse('google');
+  expect(zeroDot.parsedurl.domain).toBe('google');
+});
+
+test('it should identify port', () => {
+  const urlWithPort = mod.parse('google:80.com');
+  expect(urlWithPort.parsedurl.port).toBe('80');
+
+  const complexUrl = mod.parse('https://x.y.z.google:8000.com/x/y/z');
+  expect(complexUrl.parsedurl.port).toBe('8000');
+
+  const nullPort = mod.parse('google.com');
+  expect(nullPort.parsedurl.port).toBe(null);
+});
+
+test('it should identify path', () => {
+  const noPath = mod.parse('google.com');
+  expect(noPath.parsedurl.path).toBe('/');
+  expect(noPath.parsedurl.onlypath).toBe('/');
+
+  const goodPath = mod.parse('github.com/Marvin9/repo/master');
+  expect(goodPath.parsedurl.path).toBe('/Marvin9/repo/master');
+  expect(goodPath.parsedurl.onlypath).toBe('/Marvin9/repo/master');
+
+  const pathWithQuery = mod.parse('google.com/search?q=mod-url&location=india');
+  expect(pathWithQuery.parsedurl.path).toBe('/search?q=mod-url&location=india');
+});
+
+test('it should identify only path (no query)', () => {
+  const pathWithQuery = mod.parse('google.com/search?q=mod-url&location=india');
+  expect(pathWithQuery.parsedurl.onlypath).toBe('/search');
+});
+
+test('it should identify only query', () => {
+  const pathWithQuery = mod.parse('google.com/search?q=mod-url&location=india');
+  expect(pathWithQuery.parsedurl.query).toBe('q=mod-url&location=india');
+});
