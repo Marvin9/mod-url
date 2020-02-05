77bb57c1eca60f8ee04540ccb2d1890057123d26
Mayursinh <marvinduff97@gmail.com>
2020-02-05 14:35:47 +0530

    :white_check_mark: More integration tests
    Step towards fixing bug before v1 release

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -9,7 +9,15 @@ const invalidUrls = [
 const URLS = [
   ['google', 'https://www.google.com/'],
   ['google.com', 'http://google.uk/'],
-  ['http://google', 'http://www.google.com/']
+  ['http://google', 'http://www.google.com/'],
+  ['google.com/path', 'https://google.com/otherpath'],
+  ['google.com/path?q=query', 'https://google.com/pathv2?q=query'],
+  ['google.com/path?q=query', 'https://google.com/pathWithoutQuery'],
+  ['google.com/path?q=query', 'https://www.bing.com/path?q=query'],
+  ['google.com/path?q=query&x=y', 'https://google.com/path?q=oneQueryOnly'],
+  ['google.com/path/to/some?q=query', 'https://google.com/path/to/some?q=change&x=y'],
+  ['google.com/?q=query', 'https://google.com/?q=query'],
+  ['google.com/?q=query', 'https://x.y.google.uk/path?q=query']
 ];
 
 test('it should throw error to invalid urls', () => {
@@ -31,4 +39,28 @@ test('function should be able to modify given url and give desire url', () => {
 
   const url3 = mod.parse(URLS[2][0]).subdomain('www').done();
   expect(url3).toBe(URLS[2][1]);
+
+  const url4 = mod.parse(URLS[3][0]).path('otherpath').done();
+  expect(url4).toBe(URLS[3][1]);
+
+  const url5 = mod.parse(URLS[4][0]).onlypath('pathv2').done();
+  expect(url5).toBe(URLS[4][1]);
+
+  const url6 = mod.parse(URLS[5][0]).path('pathWithoutQuery').done();
+  expect(url6).toBe(URLS[5][1]);
+
+  const url7 = mod.parse(URLS[6][0]).subdomain('www').domain('bing').done();
+  expect(url7).toBe(URLS[6][1]);
+
+  const url8 = mod.parse(URLS[7][0]).query('q=oneQueryOnly').done();
+  expect(url8).toBe(URLS[7][1]);
+
+  const url9 = mod.parse(URLS[8][0]).query('q=change&x=y').done();
+  expect(url9).toBe(URLS[8][1]);
+
+  const url10 = mod.parse(URLS[9][0]).done();
+  expect(url10).toBe(URLS[9][1]);
+
+  const url11 = mod.parse(URLS[10][0]).subdomain('x.y').domainext('uk').onlypath('path').done();
+  expect(url11).toBe(URLS[10][1]);
 });
