50d668ca25e08605fb6c7cc6f3b6555a3009e7d6
Marvin9 <marvinduff97@gmail.com>
2020-02-08 14:49:23 +0530

    trim and check for empty string

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@ const addColonLast = (url: string): boolean => (
 );
 
 const isGarbage = (value: any, emptyString: boolean = false): boolean => (
-  value === null || value === undefined || (emptyString && typeof value === 'string' && value === '')
+  value === null || value === undefined || (emptyString && typeof value === 'string' && value.trim() === '')
 );
 
 module.exports = {
